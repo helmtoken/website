54606fdb983c3b7bb77674375be7074a2b5ed76f
helmtokenadmin <tokenhelm@proton.me>
2023-04-30 23:43:41 -0400

    update out

--- _next/static/yTlB96K1M42NyiX8k4-mP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/71-02a6f16cafc7b995.js","static/chunks/pages/index-335ad19997b608fa.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
